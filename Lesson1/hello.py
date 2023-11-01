36f9a6c8ecdd86dda048832c9e2081074bf6b321
Ramshri-Mohapatra <124039645+Ramshri-Mohapatra@users.noreply.github.com>
2023-11-01 19:01:01 +0000

    Wrote first python code

--- Lesson1/hello.py
+++ Lesson1/hello.py
@@ -1,2 +1,10 @@
-greeting ='hello World!'
-print(greeting)
+line01 = "********************"
+line02 = "*                  *"
+line03 = "*     welcome!     *"
+
+print('')
+print(line01)
+print(line02)
+print(line03)
+print(line02)
+print(line01)
